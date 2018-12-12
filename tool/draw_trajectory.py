2ca7c938a45ec4a88c7ab294b6bd4b878b187570
Wan Youwen <1756429307@qq.com>
2018-12-12 21:26:01 +0800

    Update draw_trajectory.py

--- tool/draw_trajectory.py
+++ tool/draw_trajectory.py
@@ -44,10 +44,24 @@ for line in f3:
     y3.append( float(data[2] ) -3.1192)
     z3.append( float(data[3] ) +1.4248)
 
+f4 = open("./geom.txt")
+x4 = []
+y4 = []
+z4 = []
+for line in f4:
+    if line[0] == '#':
+        continue
+    data = line.split()
+    x4.append( float(data[1] ) -0.6885)
+    y4.append( float(data[2] ) -3.1192)
+    z4.append( float(data[3] ) +1.4248)
+    
 ax = plt.subplot( 111, projection='3d')
 ax.plot(x,y,z,'r',label='groundtruth')  # 红色线条
 ax.plot(x2,y2,z2,'g',label='flow')  # 绿色线条
 ax.plot(x3,y3,z3,'b',label='src')  # 蓝色线条
+ax.plot(x4,y4,z4,'y',label='geom')  # 黄色线条
+
 ax.legend(loc='best')# 显示图例 label=' ' 
 ax.set_zlabel('Z')  # 坐标轴
 ax.set_ylabel('Y')
